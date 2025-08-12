542da243a9a03197721da776584882ce16ae1ca0
Nitika4797 <161456956+Nitika4797@users.noreply.github.com>
2025-08-12 14:57:41 +0530

    Delete backend directory

--- backend/models/Shop.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const shopSchema = new mongoose.Schema({
-  shop: { type: String, unique: true, index: true },
-  accessToken: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Shop', shopSchema);
